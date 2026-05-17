63727062c10cd601af3d4c926d965fc23f0abcfa
Gary Verhaegen <gary.verhaegen@gmail.com>
2026-05-17 19:09:39 +0200

    decide on lima instead of vagrant (#5)

--- shell.nix
+++ shell.nix
@@ -4,12 +4,11 @@ in
 pkgs.mkShell {
   LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
   buildInputs = with pkgs; [
+    babashka
     bash
     cacert
     curl
     jq
-    qemu
-    vagrant
+    lima
   ];
-  QEMU_PATH = pkgs.qemu;
 }
