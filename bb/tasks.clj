d9797409e3b01a09aeb03bdd8358a33716f687a9
Gary Verhaegen <gary.verhaegen@gmail.com>
2026-08-07 08:27:44 +0200

    request permission to use 127.0.0.2
    Turns out not just any random program can decide to create an IP for
    itself. We have to use sudo to allow the IP first.
    
    This commit documents that in README and adds a print of the command (I
    don't trust a script to run `sudo` on my behalf). It also tweaks the
    port forwarding configuration a bit.

--- bb/tasks.clj
+++ bb/tasks.clj
@@ -1,10 +1,24 @@
 (ns tasks
-  (:require [babashka.process :as p]
+  (:require [clojure.string :as str]
+            [babashka.process :as p]
             [babashka.tasks :as tasks]))
+
+(def host-loopback-ip "127.0.0.2")
+
+(defn- require-host-loopback []
+  (let [lo0 (:out @(p/process ["/sbin/ifconfig" "lo0"] {:out :string}))]
+    (when-not (str/includes? lo0 (str "inet " host-loopback-ip " "))
+      (binding [*out* *err*]
+        (println host-loopback-ip "is not configured on the host loopback interface.")
+        (println "Run this command, then retry `bb up`:")
+        (println)
+        (println "  sudo /sbin/ifconfig lo0 alias" host-loopback-ip "up"))
+      (System/exit 1))))
 
 (defn up
   "Start the claude-jail Lima VM."
   []
+  (require-host-loopback)
   (tasks/shell "limactl start --tty=false --name=claude-jail claude-jail.yaml")
   (-> (p/process ["cat" "./ssh/key"])
       (p/process ["limactl" "shell" "claude-jail" "bash" "-c"
